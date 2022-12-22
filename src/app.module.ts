a262685ec45253781d1fc95a243d6ea292108e30
cavishek39 <cavishek39@gmail.com>
2022-12-22 22:56:02 +0530

    code reafactored

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
+      autoSchemaFile: 'schema.graphql',
     }),
     UserModule,
     DatabaseModule,
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const PORT = 3000;
+
 async function bootstrap() {
+  console.log(
+    `Listen on port ${PORT} or mongo conn string: ${process.env.MONO_DB_CONNECTION_STRING}`,
+  );
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
